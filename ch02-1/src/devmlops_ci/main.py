007dd4d28b43c20655a8e1583e41d7ca58a0aec8
Lee Yeong Yeon <aktld101@naver.com>
2026-07-20 10:25:07 +0900

    Remove

--- ch02-1/src/devmlops_ci/main.py
+++
@@ -1,5 +0,0 @@
-from fastapi import FastAPI
-app = FastAPI()
-@app.get("/health")
-def health_check():
- return {"status": "ok"}
--- ch02-2/src/devmlops_ci/calculator.py
+++
@@ -1,4 +0,0 @@
-def add(a: int, b: int) -> int:
- return a + b
-def subtract(a: int, b: int) -> int:
- return a - b
