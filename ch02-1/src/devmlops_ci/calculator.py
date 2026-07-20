5ca7148c28e961cf4aad0a6aacce9a6ffb8681ea
Lee Yeong Yeon <aktld101@naver.com>
2026-07-20 10:13:35 +0900

    Moving exist files

---
+++ ch02-1/src/devmlops_ci/calculator.py
@@ -0,0 +1,4 @@
+def add(a: int, b: int) -> int:
+ return a + b
+def subtract(a: int, b: int) -> int:
+ return a - b
---
+++ ch02-2/tests/test_calculator.py
@@ -0,0 +1,7 @@
+from devmlops_ci.calculator import add, subtract
+def test_add() -> None:
+ assert add(2, 3) == 5
+def test_add_negative() -> None:
+ assert add(-1, 1) == 0
+def test_subtract() -> None:
+ assert subtract(5, 3) == 2
